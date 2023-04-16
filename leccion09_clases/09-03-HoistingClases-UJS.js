bed53f12d298752aebb7af1060b32adc1f61d83a
Dario Gimenez <dgimenez.developer@gmail.com>
2023-04-16 11:58:18 -0300

    Hoisting y clases

---
+++ leccion09_clases/09-03-HoistingClases-UJS.js
@@ -0,0 +1,33 @@
+// NO es posible crear objetos antes de declarar la clase
+//NO se aplica el concepto de Hoisting
+//let persona2 = new Persona('Karla', 'Juarez');
+
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+}
+
+let persona1 = new Persona('Juan','Perez');
+persona1.nombre = 'Juan Carlos';// set nombre('juan Carlos')
+console.log(persona1.nombre);//get nombre
+
+let persona2 = new Persona('Karla', 'Juarez');
+console.log(persona2);
